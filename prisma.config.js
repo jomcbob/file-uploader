9b7d0a6672a50bddd286e8f9bc113034377def97
Bob <andrew@risertech.com>
2025-12-19 13:35:55 -0700

    added client in prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -3,9 +3,15 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
+  client: {
+    output: "node_modules/.prisma/client",
+  },
+
   migrations: {
     path: "prisma/migrations",
   },
+
   datasource: {
     url: env("DATABASE_URL"),
   },
